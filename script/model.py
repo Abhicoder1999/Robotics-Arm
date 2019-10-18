6fb3bf257f154fb3050fcfa8e273a065e87e16aa
Abhijeet Tripathy <abhijeetofficial1999@gmail.com>
2019-10-19 02:56:02 +0530

    model completed

--- script/model.py
+++ script/model.py
@@ -1,43 +1,41 @@
-import cv2
-from google.colab import drive
-import glob
-import numpy as np
-#from matplotlib import pyplot as plt
+weight_decay = 1e-4
 
-drive.mount('/content/drive', force_remount = True)
+num_classes = 6
 
+model = Sequential()
 
-def label_it(y_train,path):
-    if path[-6] == "0":
-        y_train.append([0,0,0,0,0,1])
-    if path[-6] == "1":
-        y_train.append([0,0,0,0,1,0])
-    if path[-6] == "2":
-        y_train.append([0,0,0,1,0,0])
-    if path[-6] == "3":
-        y_train.append([0,0,1,0,0,0])
-    if path[-6] == "4":
-        y_train.append([0,1,0,0,0,0])
-    if path[-6] == "5":
-        y_train.append([1,0,0,0,0,0])
-    
+model.add(Conv2D(64, (4,4), padding='same', kernel_regularizer=regularizers.l2(weight_decay), input_shape=(128,128,1)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, (4,4), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
 
-img_path = glob.glob("/content/drive/My Drive/modified/*.png")
-y_train = []
-x_train = []
+model.add(Conv2D(128, (4,4), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (4,4), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
 
-for path in img_path:
-  image = cv2.imread(path)
-  x_train.append(image)
-  label_it(y_train,path)
-  
+model.add(Conv2D(128, (4,4), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (4,4), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
 
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-x_train_gr = np.zeros([len(x_train),128,128])
-for k in range(len(x_train)):
-  temp = x_train[k]
-  temp = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
-  temp[temp == 14] = 0 #corresponding during image storing
-  temp[temp == 38] = 1
-  x_train_gr[k] = temp
+model.add(Flatten())
+model.add(Dense(128, activation="sigmoid"))
+model.add(Activation('relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(0.0003), metrics=['accuracy'])
+
+model.summary()
